433275630756a4ec05d8882c858e60198dcb86a2
anoojgandham <anoojgandham@gmail.com>
2021-08-19 11:59:01 +0530

    Added script for inserting urls to database

--- scrapeGithub/inserUrls.py
+++ scrapeGithub/inserUrls.py
@@ -1,6 +1,10 @@
 from database import connectToDatabase, getAllUrlsToBeScraped, addUrlsToBeScraped, markUrlAsScraped
 import sys
 
+
+if len(sys.argv) > 2:
+    print("Recieved more than 1 argument, required 1. Correct usage is python insertUrls.py <filename.txt>")
+    exit()
 urlsToBeAdded = open(sys.argv[1]).read().split()
 
 conn = connectToDatabase("../db.sqlite3")
